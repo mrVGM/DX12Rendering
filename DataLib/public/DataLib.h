a5e682c628fac321a9ba1084bb74012a4656e2fb
Vasil <magaranov@abv.bg>
2023-05-01 01:39:14 +0300

    added the data lib

---
+++ DataLib/public/DataLib.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "json.hpp"
+
+#include <string>
+#include <vector>
+
+namespace data
+{
+	class DataLib
+	{
+		std::string m_data;
+		nlohmann::json m_json;
+		std::vector<nlohmann::json> m_fileEntries;
+
+	public:
+		DataLib(const char* dir, const char* name = "lib.json");
+		inline bool IsValid() const { return m_data.length() > 0; }
+		nlohmann::json GetFileEntry(const char* id) const;
+		const std::vector<nlohmann::json>& GetFileEntries () const;
+
+		std::string ReadFileById(const char* id);
+		std::string ReadFileByPath(std::string path);
+
+		void WriteBinFile(const char* id, const void *data, size_t size);
+		size_t ReadBinFile(const char* id, void *data, size_t size);
+	};
+
+	bool Init(const char* dir);
+	void Deinit();
+
+	DataLib& GetLibrary();
+}
