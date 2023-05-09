6c9c064ce49cb14a988c8baf6489c9bd638b466b
Vasko <magaranov@abv.bg>
2023-05-09 20:59:49 +0300

    gbuffer texture barriers

--- Rendering/private/RenderPass/DXDeferredRP.cpp
+++ Rendering/private/RenderPass/DXDeferredRP.cpp
@@ -26,15 +26,15 @@ rendering::DXDeferredRP::DXDeferredRP() :
     DXDevice* device = utils::GetDevice();
 
     THROW_ERROR(
-        device->GetDevice().CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_commandAllocator)),
+        device->GetDevice().CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_endListAllocator)),
         "Can't create Command Allocator!")
 
     THROW_ERROR(
-        device->GetDevice().CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_commandAllocator.Get(), nullptr, IID_PPV_ARGS(&m_commandList)),
+        device->GetDevice().CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_endListAllocator.Get(), nullptr, IID_PPV_ARGS(&m_endList)),
         "Can't reset Command List!")
 
     THROW_ERROR(
-        m_commandList->Close(),
+        m_endList->Close(),
         "Can't close Command List!")
 
     {
@@ -100,6 +100,9 @@ rendering::DXDeferredRP::DXDeferredRP() :
             device->GetDevice().CreateGraphicsPipelineState(&psoDesc, IID_PPV_ARGS(&m_pipelineState)),
             "Can't create Graphics Pipeline State!")
     }
+
+    CreateRTVHeap();
+    CreateSRVHeap();
 }
 
 rendering::DXDeferredRP::~DXDeferredRP()
@@ -111,37 +114,142 @@ D3D12_CPU_DESCRIPTOR_HANDLE rendering::DXDeferredRP::GetDescriptorHandleFor(GBuf
     return D3D12_CPU_DESCRIPTOR_HANDLE();
 }
 
-void rendering::DXDeferredRP::Prepare()
+void rendering::DXDeferredRP::CreateRTVHeap()
+{
+    using Microsoft::WRL::ComPtr;
+
+    DXDevice* device = utils::GetDevice();
+
+    // Create descriptor heaps.
+    {
+        // Describe and create a render target view (RTV) descriptor heap.
+        D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+        rtvHeapDesc.NumDescriptors = 3;
+        rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        THROW_ERROR(
+            device->GetDevice().CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvHeap)),
+            "Can't create a descriptor heap!")
+
+            m_rtvDescriptorSize = device->GetDevice().GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    }
+
+    // Create frame resources.
+    {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvHeap->GetCPUDescriptorHandleForHeapStart());
+
+        device->GetDevice().CreateRenderTargetView(rendering::deferred::GetGBufferDiffuseTex()->GetTexture(), nullptr, rtvHandle);
+        rtvHandle.Offset(1, m_rtvDescriptorSize);
+
+        device->GetDevice().CreateRenderTargetView(rendering::deferred::GetGBufferNormalTex()->GetTexture(), nullptr, rtvHandle);
+        rtvHandle.Offset(1, m_rtvDescriptorSize);
+
+        device->GetDevice().CreateRenderTargetView(rendering::deferred::GetGBufferPositionTex()->GetTexture(), nullptr, rtvHandle);
+        rtvHandle.Offset(1, m_rtvDescriptorSize);
+    }
+
+}
+
+void rendering::DXDeferredRP::CreateSRVHeap()
+{
+    using Microsoft::WRL::ComPtr;
+
+    DXDevice* device = utils::GetDevice();
+
+    // Create descriptor heaps.
+    {
+        // Describe and create a render target view (RTV) descriptor heap.
+        D3D12_DESCRIPTOR_HEAP_DESC srvHeapDesc = {};
+        srvHeapDesc.NumDescriptors = 3;
+        srvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+        srvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+        THROW_ERROR(
+            device->GetDevice().CreateDescriptorHeap(&srvHeapDesc, IID_PPV_ARGS(&m_srvHeap)),
+            "Can't create a descriptor heap!")
+
+        m_srvDescriptorSize = device->GetDevice().GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+    }
+
+    // Create frame resources.
+    {
+        CD3DX12_CPU_DESCRIPTOR_HANDLE srvHandle(m_srvHeap->GetCPUDescriptorHandleForHeapStart());
+
+        {
+            DXTexture* tex = deferred::GetGBufferDiffuseTex();
+            D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+            srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+            srvDesc.Format = tex->GetTextureDescription().Format;
+            srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+            srvDesc.Texture3D.MipLevels = 1;
+            srvDesc.Texture2D.MipLevels = 1;
+
+            device->GetDevice().CreateShaderResourceView(tex->GetTexture(), &srvDesc, srvHandle);
+            srvHandle.Offset(1, m_srvDescriptorSize);
+        }
+
+        {
+            DXTexture* tex = deferred::GetGBufferNormalTex();
+            D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+            srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+            srvDesc.Format = tex->GetTextureDescription().Format;
+            srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+            srvDesc.Texture3D.MipLevels = 1;
+            srvDesc.Texture2D.MipLevels = 1;
+
+            device->GetDevice().CreateShaderResourceView(tex->GetTexture(), &srvDesc, srvHandle);
+            srvHandle.Offset(1, m_srvDescriptorSize);
+        }
+
+        {
+            DXTexture* tex = deferred::GetGBufferPositionTex();
+            D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+            srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+            srvDesc.Format = tex->GetTextureDescription().Format;
+            srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+            srvDesc.Texture3D.MipLevels = 1;
+            srvDesc.Texture2D.MipLevels = 1;
+
+            device->GetDevice().CreateShaderResourceView(tex->GetTexture(), &srvDesc, srvHandle);
+            srvHandle.Offset(1, m_srvDescriptorSize);
+        }
+    }
+}
+
+void rendering::DXDeferredRP::PrepareEndList()
 {
     DXDevice* device = utils::GetDevice();
     DXSwapChain* swapChain = utils::GetSwapChain();
-    DXBuffer* camBuff = utils::GetCameraBuffer();
-
-    ID3D12Resource* curRT = swapChain->GetCurrentRenderTarget();
 
     THROW_ERROR(
-        m_commandAllocator->Reset(),
+        m_endListAllocator->Reset(),
         "Can't reset Command Allocator!")
 
     THROW_ERROR(
-        m_commandList->Reset(m_commandAllocator.Get(), m_pipelineState.Get()),
+        m_endList->Reset(m_endListAllocator.Get(), m_pipelineState.Get()),
         "Can't reset Command List!")
 
     {
-        CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(swapChain->GetCurrentRenderTarget(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
-        m_commandList->ResourceBarrier(1, &barrier);
+        CD3DX12_RESOURCE_BARRIER barrier[] =
+        {
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(swapChain->GetCurrentRenderTarget(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferDiffuseTex()->GetTexture(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferNormalTex()->GetTexture(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferPositionTex()->GetTexture(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE),
+        };
+        m_startList->ResourceBarrier(_countof(barrier), barrier);
     }
 
-    m_commandList->SetGraphicsRootSignature(m_rootSignature.Get());
 
-    m_commandList->RSSetViewports(1, &swapChain->GetViewport());
-    m_commandList->RSSetScissorRects(1, &swapChain->GetScissorRect());
+    m_endList->SetGraphicsRootSignature(m_rootSignature.Get());
+
+    m_endList->RSSetViewports(1, &swapChain->GetViewport());
+    m_endList->RSSetScissorRects(1, &swapChain->GetScissorRect());
 
     D3D12_CPU_DESCRIPTOR_HANDLE handles[] =
     {
         swapChain->GetCurrentRTVDescriptor()
     };
-    m_commandList->OMSetRenderTargets(_countof(handles), handles, FALSE, nullptr);
+    m_endList->OMSetRenderTargets(_countof(handles), handles, FALSE, nullptr);
 
     D3D12_VERTEX_BUFFER_VIEW vertexBufferViews[1];
     D3D12_VERTEX_BUFFER_VIEW& realVertexBufferView = vertexBufferViews[0];
@@ -150,25 +258,62 @@ void rendering::DXDeferredRP::Prepare()
     realVertexBufferView.StrideInBytes = vertexBuffer->GetStride();
     realVertexBufferView.SizeInBytes = vertexBuffer->GetBufferSize();
 
-    m_commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    m_commandList->IASetVertexBuffers(0, _countof(vertexBufferViews), vertexBufferViews);
+    m_endList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    m_endList->IASetVertexBuffers(0, _countof(vertexBufferViews), vertexBufferViews);
 
-    m_commandList->DrawInstanced(6, 1, 0, 0);
+    m_endList->DrawInstanced(6, 1, 0, 0);
 
     {
-        CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(swapChain->GetCurrentRenderTarget(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
-        m_commandList->ResourceBarrier(1, &barrier);
+        CD3DX12_RESOURCE_BARRIER barrier[] =
+        {
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(swapChain->GetCurrentRenderTarget(), D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferDiffuseTex()->GetTexture(), D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, D3D12_RESOURCE_STATE_PRESENT),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferNormalTex()->GetTexture(), D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, D3D12_RESOURCE_STATE_PRESENT),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferPositionTex()->GetTexture(), D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE, D3D12_RESOURCE_STATE_PRESENT),
+        };
+        m_startList->ResourceBarrier(_countof(barrier), barrier);
     }
 
     THROW_ERROR(
-        m_commandList->Close(),
+        m_endList->Close(),
         "Can't close Command List!")
+}
+
+void rendering::DXDeferredRP::PrepareStartList()
+{
+    DXDevice* device = utils::GetDevice();
+
+    THROW_ERROR(
+        m_startListAllocator->Reset(),
+        "Can't reset Command Allocator!")
+
+    THROW_ERROR(
+        m_startList->Reset(m_startListAllocator.Get(), m_pipelineState.Get()),
+        "Can't reset Command List!")
+
+    {
+        CD3DX12_RESOURCE_BARRIER barrier[] =
+        {
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferDiffuseTex()->GetTexture(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferNormalTex()->GetTexture(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET),
+            CD3DX12_RESOURCE_BARRIER::CD3DX12_RESOURCE_BARRIER::Transition(deferred::GetGBufferPositionTex()->GetTexture(), D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET),
+        };
+        m_startList->ResourceBarrier(_countof(barrier), barrier);
+    }
+
+    THROW_ERROR(
+        m_startList->Close(),
+        "Can't close Command List!")
+}
+
+void rendering::DXDeferredRP::Prepare()
+{
 }
 
 void rendering::DXDeferredRP::Execute()
 {
     DXCommandQueue* commandQueue = rendering::utils::GetCommandQueue();
-    ID3D12CommandList* ppCommandLists[] = { m_commandList.Get() };
+    ID3D12CommandList* ppCommandLists[] = { m_endList.Get() };
     commandQueue->GetCommandQueue()->ExecuteCommandLists(_countof(ppCommandLists), ppCommandLists);
 }
 
--- Rendering/private/RenderPass/DXDeferredRP.h
+++ Rendering/private/RenderPass/DXDeferredRP.h
@@ -23,15 +23,29 @@ namespace rendering
 		};
 
 	private:
-		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_commandAllocator;
-		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_commandList;
+		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_endListAllocator;
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_endList;
+
+		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_startListAllocator;
+		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_startList;
 
 		Microsoft::WRL::ComPtr<ID3D12PipelineState> m_pipelineState;
 		Microsoft::WRL::ComPtr<ID3D12RootSignature> m_rootSignature;
 
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_rtvHeap;
+		Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_srvHeap;
+
+		UINT m_rtvDescriptorSize = 0;
+		UINT m_srvDescriptorSize = 0;
+
 		const DXShader& m_vertexShader;
 		const DXShader& m_pixelShader;
 
+		void CreateRTVHeap();
+		void CreateSRVHeap();
+
+		void PrepareEndList();
+		void PrepareStartList();
 	public:
 		DXDeferredRP();
 		virtual ~DXDeferredRP();
