8b02c6175c0bc6a993260b3f0bcef2e52ecbfad3
Vasil <magaranov@abv.bg>
2023-05-28 01:58:00 +0300

    removed some code from the lights manager

--- DeferredRP/private/LightsManager.h
+++ DeferredRP/private/LightsManager.h
@@ -25,29 +25,13 @@ namespace rendering
 
 		std::vector<Light> m_lights;
 		DXBuffer* m_lightsBuffer = nullptr;
-		DXTexture* m_shadowMap = nullptr;
-		DXTexture* m_shadowMapDepthStencil = nullptr;
-		DXBuffer* m_shadowMapSettingsBuffer = nullptr;
-
-		rendering::DXDescriptorHeap* m_shadowMapDSDescriptorHeap = nullptr;
-		rendering::DXDescriptorHeap* m_shadowMapRTV = nullptr;
-		rendering::DXDescriptorHeap* m_shadowMapSRV = nullptr;
-
-		void LoadShadowMapTex(jobs::Job* done);
-		void LoadShadowMapDSTex(jobs::Job* done);
-		void LoadShadowMapSettingsBuffer(jobs::Job* done);
-
-		void CreateDescriptorHeaps();
+		
 	public:
 		LightsManager();
 		virtual ~LightsManager();
 
 		void AddLight(const Light& light);
 		void LoadLightsBuffer(jobs::Job* done);
-		void LoadShadowMap(jobs::Job* done);
-
-		void UpdateShadowMapSettings();
-
 		DXBuffer* GetLightsBuffer();
 
 		const Light& GetLight(int index) const;
